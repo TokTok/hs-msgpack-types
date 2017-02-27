f30332a6f8bf95184096e59d67ce65ce5f9030ca
Anton Kondrachkov <sx91@protonmail.com>
2017-02-27 17:30:11 +0300

    Add Option type
    Signed-off-by: Anton Kondrachkov <sx91@protonmail.com>

--- src/Data/MessagePack/Types.hs
+++ src/Data/MessagePack/Types.hs
@@ -11,3 +11,4 @@ import           Data.MessagePack.Types.Class     as X
 import           Data.MessagePack.Types.Generic   ()
 import           Data.MessagePack.Types.Instances ()
 import           Data.MessagePack.Types.Object    as X
+import           Data.MessagePack.Types.Option    as X
