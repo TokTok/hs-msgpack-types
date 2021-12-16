9148e4f28ccdd15ccc4053a4f69c3409d3f9d28a
iphydf <iphydf@users.noreply.github.com>
2021-12-16 01:51:47 +0000

    Revert "translate type products as linear arrays"
    This reverts commit 8d993fc04781477cfa865690e637a0097e4be9a4.
    
    It breaks a test in hs-msgpack-binary.

--- src/Data/MessagePack/Types/Generic.hs
+++ src/Data/MessagePack/Types/Generic.hs
@@ -1,27 +1,24 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
-{-# OPTIONS_GHC -Wno-simplifiable-class-constraints #-}
-{-# LANGUAGE CPP                  #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE IncoherentInstances  #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE LambdaCase           #-}
-{-# LANGUAGE Safe                 #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE StrictData           #-}
-{-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE IncoherentInstances #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE Safe                #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StrictData          #-}
+{-# LANGUAGE TypeOperators       #-}
 module Data.MessagePack.Types.Generic () where
 
-import           Control.Applicative              (Applicative, (<$>), (<*>))
-import           Control.Monad                    ((>=>))
-import           Control.Monad.Trans.State.Strict (StateT, evalStateT, get, put)
-import           Data.Bits                        (shiftR)
-import           Data.Word                        (Word64)
+import           Control.Applicative           (Applicative, (<$>), (<*>))
+import           Control.Monad                 ((>=>))
+import           Data.Bits                     (shiftR)
+import           Data.Word                     (Word64)
 import           GHC.Generics
 
 import           Data.MessagePack.Types.Class
-import           Data.MessagePack.Types.Object    (Object (..))
+import           Data.MessagePack.Types.Object (Object (..))
 
 instance GMessagePack V1 where
     gToObject = undefined
@@ -32,11 +29,9 @@ instance GMessagePack U1 where
     gFromObject ObjectNil = return U1
     gFromObject _         = fail "invalid encoding for custom unit type"
 
-instance GProdPack a => GMessagePack a where
+instance (GMessagePack a, GProdPack b) => GMessagePack (a :*: b) where
     gToObject   = toObject . prodToObject
-    gFromObject o = do
-        list <- fromObject o
-        evalStateT prodFromObject list
+    gFromObject = fromObject >=> prodFromObject
 
 instance (GSumPack a, GSumPack b, SumSize a, SumSize b) => GMessagePack (a :+: b) where
     gToObject = sumToObject 0 size
@@ -67,25 +62,19 @@ class GProdPack f where
            , MonadFail m
 #endif
            )
-        => StateT [Object] m (f a)
+        => [Object]
+        -> m (f a)
 
 
-instance (GProdPack a, GProdPack b) => GProdPack (a :*: b) where
-    prodToObject (a :*: b) = prodToObject a ++ prodToObject b
-    prodFromObject = do
-        f <- prodFromObject
-        g <- prodFromObject
-        pure $ f :*: g
+instance (GMessagePack a, GProdPack b) => GProdPack (a :*: b) where
+    prodToObject (a :*: b) = gToObject a : prodToObject b
+    prodFromObject (a : b) = (:*:) <$> gFromObject a <*> prodFromObject b
+    prodFromObject []      = fail "invalid encoding for product type"
 
 instance GMessagePack a => GProdPack (M1 t c a) where
     prodToObject (M1 x) = [gToObject x]
-    prodFromObject = do
-        objs <- get
-        case objs of
-            (x:xs) -> do
-                put xs
-                M1 <$> gFromObject x
-            _      -> fail "invalid encoding for product type"
+    prodFromObject [x] = M1 <$> gFromObject x
+    prodFromObject _   = fail "invalid encoding for product type"
 
 
 -- Sum type packing.
