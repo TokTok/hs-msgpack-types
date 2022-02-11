a6641c077cec18b0b6b979ed8f4a171c11c902c1
iphydf <iphydf@users.noreply.github.com>
2022-02-11 22:16:27 +0000

    refactor: Use `MonadValidate` for parsing.
    `MonadFail` is not defined well on `Either`, making it not terribly
    useful. Also, `MonadValidate` allows us to collect multiple error
    messages.

--- src/Data/MessagePack/Types.hs
+++ src/Data/MessagePack/Types.hs
@@ -3,9 +3,12 @@ module Data.MessagePack.Types
     ( Assoc (..)
     , Object (..)
     , MessagePack (..)
+    , DecodeError, decodeError, errorMessages
     ) where
 
-import           Data.MessagePack.Types.Assoc   (Assoc (..))
-import           Data.MessagePack.Types.Class   (MessagePack (..))
-import           Data.MessagePack.Types.Generic ()
-import           Data.MessagePack.Types.Object  (Object (..))
+import           Data.MessagePack.Types.Assoc       (Assoc (..))
+import           Data.MessagePack.Types.Class       (MessagePack (..))
+import           Data.MessagePack.Types.DecodeError (DecodeError, decodeError,
+                                                     errorMessages)
+import           Data.MessagePack.Types.Generic     ()
+import           Data.MessagePack.Types.Object      (Object (..))
