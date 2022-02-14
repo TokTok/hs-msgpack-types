a742376d506f4a79ebee4eaad46487d3bdb6a98b
iphydf <iphydf@users.noreply.github.com>
2022-02-14 18:08:06 +0000

    cleanup: Remove Arbitrary instance from main library.
    Reducing dependencies.

--- src/Data/MessagePack/Types/Object.hs
+++ src/Data/MessagePack/Types/Object.hs
@@ -16,8 +16,6 @@ import           Data.Typeable             (Typeable)
 import qualified Data.Vector               as V
 import           Data.Word                 (Word64, Word8)
 import           GHC.Generics              (Generic)
-import           Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
-import qualified Test.QuickCheck.Gen       as Gen
 
 
 -- | Object Representation of MessagePack data.
@@ -48,19 +46,3 @@ data Object
     deriving (Read, Show, Eq, Ord, Typeable, Generic)
 
 instance NFData Object
-
-instance Arbitrary Object where
-    arbitrary = Gen.sized $ \n -> Gen.oneof
-        [ pure ObjectNil
-        , ObjectBool   <$> arbitrary
-        , ObjectInt    <$> negatives
-        , ObjectWord   <$> arbitrary
-        , ObjectFloat  <$> arbitrary
-        , ObjectDouble <$> arbitrary
-        , ObjectStr    <$> (T.pack <$> arbitrary)
-        , ObjectBin    <$> (S.pack <$> arbitrary)
-        , ObjectArray  <$> (V.fromList <$> Gen.resize (n `div` 2) arbitrary)
-        , ObjectMap    <$> (V.fromList <$> Gen.resize (n `div` 4) arbitrary)
-        , ObjectExt    <$> arbitrary <*> (S.pack <$> arbitrary)
-        ]
-        where negatives = Gen.choose (minBound, -1)
